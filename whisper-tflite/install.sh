aec911c09afa6b0cd8e08301e861454e25fba97c
Florian Quirin <info@bytemind.de>
2023-01-17 19:37:19 +0100

    more comments and checks

--- whisper-tflite/install.sh
+++ whisper-tflite/install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+echo "Installing Whisper-TFlite ..."
 sudo apt update
 sudo apt install -y --no-install-recommends python3-pip python3-dev python3-setuptools python3-wheel python3-venv
 sudo apt install -y --no-install-recommends ffmpeg
-if [ -f "venv" ]; then
+if [ -d "venv/" ]; then
+	echo "Activating Python virtual env."
 	source venv/bin/activate
 else
+	echo "Creating and activating Python virtual env."
 	python3 -m venv venv && source venv/bin/activate
 fi
+echo "Installing packages ..."
 pip3 install --upgrade pip
 pip3 install git+https://github.com/openai/whisper.git
 pip3 install tensorflow==2.11.0 tflite==2.10.0 tensorflow_io==0.27.0
 #pip3 install tflite_runtime
+echo "Downloading models ..."
 mkdir -p models
 cd models
 if [ ! -f "whisper.tflite" ]; then
--- whisper-tflite/run-test.sh
+++ whisper-tflite/run-test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -d "venv/" ]; then
+	echo "Please make sure you've activated the Python virtual environment!"
+	echo "Use: source venv/bin/activate"
+fi
 start_t=$(date +"%s.%N")
 time python3 test.py
 finish_t=$(date +"%s.%N")
